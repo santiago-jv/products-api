97bd0568385bc8a0afa1212139786725dc8c1dd6
santiago-jv <santiagoolayojv@gmail.com>
2022-06-11 07:26:14 -0500

    first commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,14 +5,16 @@ const {
     DATABASE_PORT,
     DATABASE_USER,
     DATABASE_PASSWORD,
-    DATABASE_NAME
+    DATABASE_NAME,
+    DATABASE_HOST
 } = process.env
 
 const client = new Client({
     database:DATABASE_NAME,
     user:DATABASE_USER,
     port:DATABASE_PORT,
-    password:DATABASE_PASSWORD
+    password:DATABASE_PASSWORD,
+    host:DATABASE_HOST
 })
 client.connect()
 
