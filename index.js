c0f1b294230ef0c5ff2eeff6b9d1890ec24a27a7
santiago-jv <santiagoolayojv@gmail.com>
2022-06-11 07:48:30 -0500

    cors config

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const router = require('./src/routers');
 const server = express()
-
+const cors = require('cors');
+server.use(cors())
 server.use(express.json())
 server.use('/api',router)
 
